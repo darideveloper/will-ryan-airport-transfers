0ee36959fda461870e64362004bdc82848d775e4
darideveloper <darideveloper@gmail.com>
2023-04-27 15:53:32 -0600

    fix (hero): responsive

--- src/sections/hero.jsx
+++ src/sections/hero.jsx
@@ -12,17 +12,19 @@ export default function Hero () {
       </div>
 
       <div className="hero-image relative">
-        <img src="/imgs/hero.png" alt="transport image" className="w-full"/>
-        <div className="text w-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 text-center flex flex-col items-center justify-center">
-          <h1 className='text-xl py-2 px-4 sm:text-4xl leading-snug bg-white inline-block lg:text-5xl xl:text-6xl lg:py-5 lg:px-20 '>
+
+        <div className="text-wrapper mt-16 mb-10 w-full z-10 text-center flex flex-col items-center justify-center sm:absolute sm:top-1/2 sm:left-1/2 sm:-translate-x-1/2 sm:-translate-y-1/2 sm:m-0">
+          <h1 className='py-2 px-4 text-4xl leading-snug bg-white inline-block lg:text-5xl xl:text-6xl lg:py-5 lg:px-20 '>
             PLEASE BOOK NOW AND SECURE YOUR 
             <br />
             AIRPORT TRANSFER TO YOUR HOTE
           </h1>
-          <h2 className='text-lg py-1 px-2 sm:text-2xl -mt-2 bg-white inline-block lg:text-2xl xl:text-3xl lg:py-5 lg:px-20 '>
+          <h2 className='lg py-1 px-2 text-2xl -mt-2 bg-white inline-block lg:text-2xl xl:text-3xl lg:py-5 lg:px-20 '>
             We will be happy to provide Airport Transportation
           </h2>
         </div>
+
+        <img src="/imgs/hero.png" alt="transport image" className="w-full"/>
       </div>
     </section>
   )
