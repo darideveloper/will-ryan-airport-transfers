95053f83398fd9a9461182167645a27a4d972966
darideveloper <darideveloper@gmail.com>
2023-04-27 15:55:52 -0600

    fix (header): padding in responsive

--- src/sections/header.jsx
+++ src/sections/header.jsx
@@ -4,7 +4,7 @@ import Menu from '../assets/menu'
 export default function Header () {
 
   return (
-    <header className="bg-blue pt-6 pb-0 sm:pb-5">
+    <header className="bg-blue pt-6 pb-0 sm:pb-5 md:py-5">
       <div className="container flex flex-col items-center justify-center lg:flex-row lg:justify-between">
         
         <Logo 
