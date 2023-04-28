26e06f831a1fa5d2d9a0761e49e9dffbc5ec0e79
darideveloper <darideveloper@gmail.com>
2023-04-28 00:33:38 -0600

    feat (tailwind.config): custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./src/*.{html,jsx}", "./src/**/*.{html,jsx}", "./index.html",],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "blue": "#4F6A9E",
+        "black": "#1a1a1a",
+        "white": "#ffffff",
+        "gold": "#C8B568"
+      }
+    },
   },
   plugins: [],
 }
