b0ed78ab41dff1f1fb0192a1c75ebfd6b0b9889b
darideveloper <darideveloper@gmail.com>
2023-04-28 17:48:43 -0600

    feat (input): required inputs by default

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,10 +1,17 @@
 import PropTypes from 'prop-types'
 
-export default function Input ({label, placeholder="", type, name, value, handleUpdate}) {
+export default function Input({ label, placeholder = "", type, name, value, handleUpdate, isRequired = true }) {
   return (
     <label className='w-full py-2'>
       <span className='text-lg'>{label}</span>
-      <input className='block border-2 w-full px-5 h-12 mt-2 border-blue rounded-lg transition duration-300 opacity-60 focus:shadow-lg focus:opacity-100' type={type} placeholder={placeholder} name={name} value={value} onChange={(e) => handleUpdate(e)} />
+      <input 
+        className='block border-2 w-full px-5 h-12 mt-2 border-blue rounded-lg transition duration-300 opacity-60 focus:shadow-lg focus:opacity-100'
+        type={type} 
+        placeholder={placeholder} 
+        name={name} 
+        value={value} 
+        onChange={(e) => handleUpdate(e)} 
+        required={isRequired}/>
     </label>
   )
 }
@@ -16,4 +23,5 @@ Input.propTypes = {
   name: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired,
   handleUpdate: PropTypes.func.isRequired,
+  isRequired: PropTypes.bool
 }
