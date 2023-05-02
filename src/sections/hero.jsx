9657f260daa4d9d216460fc40810ba57e0e6506b
darideveloper <darideveloper@gmail.com>
2023-05-01 21:49:06 -0600

    feat (sections/hero): update main text

--- src/sections/hero.jsx
+++ src/sections/hero.jsx
@@ -14,14 +14,11 @@ export default function Hero () {
       <div className="hero-image relative">
 
         <div className="text-wrapper mt-16 mb-10 w-full z-10 text-center flex flex-col items-center justify-center sm:absolute sm:top-1/2 sm:left-1/2 sm:-translate-x-1/2 sm:-translate-y-1/2 sm:m-0">
-          <h1 className='py-2 px-4 text-4xl leading-snug bg-white inline-block lg:text-5xl xl:text-6xl lg:py-5 lg:px-20 '>
-            PLEASE BOOK NOW AND SECURE YOUR 
+          <h1 className='py-2 px-4 text-lg sm:text-2xl md:text-4xl lg:text-5xl leading-snug bg-white inline-block lg:py-5 lg:px-20 '>
+            PLEASE BOOK NOW AND SECURE AIRPORT 
             <br />
-            AIRPORT TRANSFER TO YOUR HOTE
+            TRANSPORTATION TO YOUR HOTEL OR AIRBNB
           </h1>
-          <h2 className='lg py-1 px-2 text-2xl -mt-2 bg-white inline-block lg:text-2xl xl:text-3xl lg:py-5 lg:px-20 '>
-            We will be happy to provide Airport Transportation
-          </h2>
         </div>
 
         <img src="./imgs/hero.png" alt="transport image" className="w-full"/>
