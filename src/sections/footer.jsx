0c3d6d33d821a4a4c61be82c4a59387de7ec2c23
daridev <darideveloper@gmail.com>
2023-08-08 14:40:36 -0600

    feat: change page name anc contact email

--- src/sections/footer.jsx
+++ src/sections/footer.jsx
@@ -26,8 +26,8 @@ export default function Footer() {
 
             <ContactCard
               svgPath={<path d="M22 5v14h-20v-14h20zm2-2h-24v18h24v-18zm-2 16l-6.526-6.618-3.445 3.483-3.418-3.525-6.611 6.66 5.051-8-5.051-6 10.029 7.446 9.971-7.446-4.998 6.01 4.998 7.99z" />}
-              link="mailto:info@rivieramayaairporttransfers.com"
-              text="info@rivieramayaairporttransfers.com"
+              link="mailto:omar@cancunconcierge.com.mx"
+              text="omar@cancunconcierge.com.mx"
             />
 
             <ContactCard
