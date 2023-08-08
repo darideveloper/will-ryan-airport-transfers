abaed887126ef999f6b3df4dc3d08584ff6d87e5
daridev <darideveloper@gmail.com>
2023-08-08 14:57:00 -0600

    feat (hero): update content and image

--- src/sections/hero.jsx
+++ src/sections/hero.jsx
@@ -8,13 +8,19 @@ export default function Hero () {
 
         <div className="text-wrapper mt-16 mb-10 w-full z-10 text-center flex flex-col items-center justify-center sm:absolute sm:top-1/2 sm:left-1/2 sm:-translate-x-1/2 sm:-translate-y-1/2 sm:m-0">
           <h1 className='py-2 px-4 text-lg sm:text-2xl md:text-4xl lg:text-6xl leading-snug bg-white inline-block lg:py-5 lg:px-20 '>
-            PLEASE BOOK NOW AND SECURE AIRPORT 
+            Complimentary transportation will be provided
             <br />
-            TRANSPORTATION TO YOUR HOTEL OR AIRBNB
+            <span
+              className={`
+                text-md sm:text-lg md:text-2xl lg:text-4xl
+              `}
+            >
+              TRANSPORTATION TO YOUR HOTEL OR AIRBNB
+            </span>
           </h1>
         </div>
 
-        <img src="./imgs/hero.png" alt="transport image" className="w-full"/>
+        <img src="./imgs/hero.jpg" alt="transport image" className="w-full"/>
       </div>
     </section>
   )
