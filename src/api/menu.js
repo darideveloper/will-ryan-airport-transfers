00705b0a28d1cdf63db183dfc1631948ade6aa25
daridev <darideveloper@gmail.com>
2023-08-12 12:23:06 -0600

    feat (api/menu): update hader links

--- src/api/menu.js
+++ src/api/menu.js
@@ -4,7 +4,7 @@ export async function getMenu () {
     {text: "Buy now", link: "#buy", blank:false},
     {text: "Services", link: "#services", blank:false},
     {text: "Contact us", link: "#footer", blank:false},
-    {text: "Tours", link: "http://ezbookingtours.com/", blank:true},
+    {text: "About us", link: "https://cancunconciergedmc.com/", blank:true},
 ]
 
 }
